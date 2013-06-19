b429ebdb20f2662af5e1335758fa19a145ceeeca
Hannes Walz <hannes@leuchtetgruen.de>
2013-06-19 15:30:34 +0200

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,136 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import de.leuchtetgruen.F;
+
+public class Test {
+	
+
+	public static void main(String[] args) {
+		String[] arr = { "New York", "Rio", "Tokyo", "New Mexico" };
+		List<String> c = Arrays.asList(arr);
+
+		testEach(arr,c);
+		testMap(arr, c);
+		testReduce(arr, c);
+		testFilterAndReject(arr, c);
+		testAnyAllCountValid(arr, c);
+		testSortWithoutCopy(arr, c);
+		testMinMax(arr, c);
+		testGroup(arr, c);
+	}
+
+	private static void testEach(String[] arr, List<String> c) {
+		System.out.println("Testing -> each");
+		
+		F.each(c, F.Utils.printer);
+		System.out.println("--");
+		F.each(arr, F.Utils.printer);
+	}
+
+	
+	private static void testMap(String[] arr, List<String> c) {
+		System.out.println("Testing -> map");
+		F.Mapper m = new F.Mapper() {
+			
+			@Override
+			public Object map(Object o) {
+				return (Integer) ((String) o).length();
+			}
+		};
+		
+		F.Utils.print(F.map(arr, m));
+		System.out.println("--");
+		F.Utils.print(F.map(c, m));
+	}
+	
+	private static void testReduce(String[] arr, List<String> c) {
+		System.out.println("Testing -> reduce");
+		
+		F.Reducer rTotalLength = new F.Reducer() {
+			@Override
+			public Object reduce(Object memo, Object o) {
+				return (Integer) ((Integer) memo) + ((String) o).length();
+			}
+		};
+		
+		System.out.println(F.reduce(c, rTotalLength, 0));
+		System.out.println(F.reduce(arr, rTotalLength, 0));
+	}
+	
+	private static F.Decider moreThanOneWordDecider = new F.Decider() {
+		
+		@Override
+		public boolean decide(Object o) {
+			return ((String) o).contains(" ");
+		}
+	};
+	
+	private static void testFilterAndReject(String[] arr, List<String> c) {
+		System.out.println("Testing -> filter and reject");
+		
+		F.Utils.print(F.filter(arr, moreThanOneWordDecider));
+		System.out.println("--");
+		F.Utils.print(F.filter(c, moreThanOneWordDecider));
+		System.out.println("~");
+		F.Utils.print(F.reject(arr, moreThanOneWordDecider));
+		System.out.println("--");
+		F.Utils.print(F.reject(c, moreThanOneWordDecider));
+	}
+	
+	private static void testAnyAllCountValid(String[] arr, List<String> c) {
+		System.out.println("Testing -> isValidForAny ..All and countValidEntries");
+		
+		System.out.println(F.isValidForAny(arr, moreThanOneWordDecider));
+		System.out.println(F.isValidForAny(c, moreThanOneWordDecider));
+		System.out.println("~");
+		System.out.println(F.isValidForAll(arr, moreThanOneWordDecider));
+		System.out.println(F.isValidForAll(c, moreThanOneWordDecider));
+		System.out.println("~");
+		System.out.println(F.countValidEntries(arr, moreThanOneWordDecider));
+		System.out.println(F.countValidEntries(c, moreThanOneWordDecider));
+	}
+	
+	private static Comparator<Object> stringLengthComparator = new Comparator<Object>() {
+		public int compare(Object arg0, Object arg1) {
+			int i1 = ((String) arg0).length();
+			int i2 = ((String) arg1).length();
+			if (i1==i2) return 0;
+			return (i1 > i2) ? 1 : -1 ;
+		}
+	};
+	
+	private static void testSortWithoutCopy(String[] arr, List<String> c) {
+		System.out.println("Testing -> sortWithoutCopy");
+		
+		F.Utils.print(F.sortWithoutCopy(arr, stringLengthComparator));
+		System.out.println("--");
+		F.Utils.print(F.sortWithoutCopy(c, stringLengthComparator));
+	}
+	
+	private static void testMinMax(String[] arr, List<String> c) {
+		System.out.println("Testing -> min / max");
+		
+		System.out.println(F.min(c, stringLengthComparator));
+		System.out.println(F.min(arr, stringLengthComparator));
+		System.out.println("~");
+		System.out.println(F.max(c, stringLengthComparator));
+		System.out.println(F.max(arr, stringLengthComparator));
+		
+	}
+	
+	private static void testGroup(String[] arr, List<String> c) {
+		System.out.println("Testing -> group");
+		
+		F.Mapper m = new F.Mapper() {
+			public Object map(Object o) {
+				return ((String) o).subSequence(0, 1);
+			}
+		};
+		
+		F.Utils.iterateOverGroup(F.group(arr, m), F.Utils.groupPrinter);
+		System.out.println("--");
+		F.Utils.iterateOverGroup(F.group(c, m), F.Utils.groupPrinter);
+	}
+}
---
+++ src/de/leuchtetgruen/F.java
@@ -0,0 +1,368 @@
+package de.leuchtetgruen;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * @author Hannes Walz<info@leuchtetgruen.de>
+ *
+ * This class provides some functional programming to collection/array handling in
+ * java allowing you to write much more concise code. 
+ *
+ */
+public class F {
+	// INTERFACES
+	public static interface Runner {
+		public void run(Object o);
+	};
+	
+	public static interface HashRunner {
+		public void run(Object k, Object v);
+	}
+	
+	public static interface Mapper {
+		public Object map(Object o);
+	}
+	
+	public static interface Reducer {
+		public Object reduce(Object memo, Object o);
+	}
+	
+	public static interface Decider {
+		public boolean decide(Object o);
+	}
+	
+	public static interface Cloneable {
+		public Object clone();
+	}
+	
+	
+	// EACH
+	public static void each(Collection<?> c, Runner r) {
+		for (Object o : c) {
+			r.run(o);
+		}
+	}
+	
+	public static void each(Object[] arr, Runner r) {
+		for (Object o : arr) {
+			r.run(o);
+		}
+	}
+	
+	public static void each(HashMap<Object, Object> map, HashRunner r) {
+		Set<Object> s = map.keySet();
+		for (Object k : s) {
+			r.run(k, map.get(k));
+		}
+	}
+	
+	// MAP
+	public static List<Object> map(Collection<?> c, Mapper r) {
+		ArrayList<Object> ret = new ArrayList<Object>();
+		for (Object o : c) {
+			ret.add(r.map(o));
+		}
+		return ret;
+	}
+	
+	public static Object[] map(Object[] arr, Mapper r) {
+		ArrayList<Object> ret = new ArrayList<Object>();
+		for (Object o : arr) {
+			ret.add(r.map(o));
+		}
+		return ret.toArray(new Object[ret.size()]);
+	}
+	
+	// REDUCE
+	public static Object reduce(Collection<?> c, Reducer r, Object memo) {
+		for (Object o : c) {
+			memo = r.reduce(memo, o);
+		}
+		return memo;
+	}
+	
+	public static Object reduce(Object[] arr, Reducer r, Object memo) {
+		for (Object o : arr) {
+			memo = r.reduce(memo, o);
+		}
+		return memo;
+	}
+	
+	// FILTER
+	public static List<Object> filter(Collection<?> c,Decider r) {
+		ArrayList<Object> ret = new ArrayList<Object>();
+		for (Object o: c) {
+			if (r.decide(o)) ret.add(o);
+		}
+		return ret;
+	}
+	
+	public static Object[] filter(Object[] arr, Decider r) {
+		ArrayList<Object> ret = new ArrayList<Object>();
+		for (Object o: arr) {
+			if (r.decide(o)) ret.add(o);
+		}
+		return ret.toArray(new Object[ret.size()]);
+	}
+	
+	// REJECT
+	public static List<Object> reject(Collection<?> c,Decider r) {
+		ArrayList<Object> ret = new ArrayList<Object>();
+		for (Object o: c) {
+			if (!r.decide(o)) ret.add(o);
+		}
+		return ret;
+	}
+	
+	public static Object[] reject(Object[] arr, Decider r) {
+		ArrayList<Object> ret = new ArrayList<Object>();
+		for (Object o: arr) {
+			if (!r.decide(o)) ret.add(o);
+		}
+		return ret.toArray(new Object[ret.size()]);
+	}
+	
+	// ISVALIDFORALL
+	public static boolean isValidForAll(Collection<?> c, Decider r) {
+		boolean all = true;
+		for (Object o: c) {
+			all = all && r.decide(o);
+		}
+		return all;
+	}
+	
+	public static boolean isValidForAll(Object[] arr, Decider r) {
+		boolean all = true;
+		for (Object o: arr) {
+			all = all && r.decide(o);
+		}
+		return all;
+	}
+	
+	// ISVALIDFORANY
+	public static boolean isValidForAny(Collection<?> c, Decider r) {
+		boolean all = false;
+		for (Object o: c) {
+			all = all || r.decide(o);
+		}
+		return all;
+	}
+	
+	public static boolean isValidForAny(Object[] arr, Decider r) {
+		boolean all = false;
+		for (Object o: arr) {
+			all = all || r.decide(o);
+		}
+		return all;
+	}
+	
+	// COUNTVALIDENTRIES
+	public static int countValidEntries(Collection<?> c, Decider r) {
+		int count = 0;
+		for (Object o : c) {
+			if (r.decide(o)) count++;
+		}
+		return count;
+	}
+	
+	public static int countValidEntries(Object[] arr, Decider r) {
+		int count = 0;
+		for (Object o : arr) {
+			if (r.decide(o)) count++;
+		}
+		return count;
+	}
+	
+	// SORT
+	public static List<?> sortWithoutCopy(List<?> c, Comparator<Object> r) {
+		Collections.sort(c, r);
+		return c;
+	}
+	
+	public static Object[] sortWithoutCopy(Object[] arr, Comparator<Object> r) {
+		Object[] copy = arr.clone();
+		Arrays.sort(copy, r);
+		return copy;
+	}
+	
+	// MIN
+	public static Object min(Collection<?> c, final Comparator<Object> r) {
+		Object min = null;
+		return reduce(c, new Reducer() {
+			
+			@Override
+			@SuppressWarnings("unchecked")
+			public Object reduce(Object memo, Object o) {
+				if (memo == null) return o;
+				
+				Comparable<Object> co = (Comparable<Object>) o;
+				Comparable<Object> cm = (Comparable<Object>) memo;
+				int result = r.compare(co, cm); 
+				if (result < 0) {
+					return co;
+				}
+				else {
+					return cm;
+				}
+			}
+		}, min);
+	}
+	
+	public static Object min(Object[] arr, final Comparator<Object> r) {
+		Object min = null;
+		return reduce(arr, new Reducer() {
+			
+			@Override
+			@SuppressWarnings("unchecked")
+			public Object reduce(Object memo, Object o) {
+				if (memo == null) return o;
+				
+				Comparable<Object> co = (Comparable<Object>) o;
+				Comparable<Object> cm = (Comparable<Object>) memo;
+				int result = r.compare(co, cm); 
+				if (result < 0) {
+					return co;
+				}
+				else {
+					return cm;
+				}
+			}
+		}, min);
+	}
+	
+	// MAX
+	public static Object max(Collection<?> c, final Comparator<Object> r) {
+		Object max = null;
+		return reduce(c, new Reducer() {
+			
+			@SuppressWarnings("unchecked")
+			@Override
+			public Object reduce(Object memo, Object o) {
+				if (memo == null) return o;
+				
+				Comparable<Object> co = (Comparable<Object>) o;
+				Comparable<Object> cm = (Comparable<Object>) memo;
+				int result = r.compare(co, cm); 
+				if (result > 0) {
+					return co;
+				}
+				else {
+					return cm;
+				}
+			}
+		}, max);
+	}
+	
+	public static Object max(Object[] arr, final Comparator<Object> r) {
+		Object max = null;
+		return reduce(arr, new Reducer() {
+			
+			@Override
+			@SuppressWarnings("unchecked")
+			public Object reduce(Object memo, Object o) {
+				if (memo == null) return o;
+				
+				Comparable<Object> co = (Comparable<Object>) o;
+				Comparable<Object> cm = (Comparable<Object>) memo;
+				int result = r.compare(co, cm); 
+				if (result > 0) {
+					return co;
+				}
+				else {
+					return cm;
+				}
+			}
+		}, max);
+	}
+	
+	// GROUP
+	public static HashMap<Object, Object> group(Collection<?> c, Mapper r) {
+		HashMap<Object, Object> ret = new HashMap<Object, Object>();
+		for (Object o: c) {
+			Object mapped = r.map(o);
+			@SuppressWarnings("unchecked")
+			ArrayList<Object> list = (ArrayList<Object>) ret.get(mapped);
+			if (list==null) {
+				list = new ArrayList<Object>();
+			}
+			list.add(o);
+			ret.put(mapped, list);
+		}
+		
+		return ret;
+	}
+	
+	public static HashMap<Object, Object> group(Object[] arr, Mapper r) {
+		HashMap<Object, Object> ret = new HashMap<Object, Object>();
+		for (Object o: arr) {
+			Object mapped = r.map(o);
+			@SuppressWarnings("unchecked")
+			ArrayList<Object> list = (ArrayList<Object>) ret.get(mapped);
+			if (list==null) {
+				list = new ArrayList<Object>();
+			}
+			list.add(o);
+			ret.put(mapped, list);
+		}
+		
+		return ret;
+	}
+	
+	// UTILS
+	public static class Utils {
+		public static F.Runner printer = new F.Runner() {
+			public void run(Object o) {
+				System.out.println(o);
+			}
+		};
+		
+		public static void print(Collection<?> c) {
+			each(c, printer);
+		}
+		
+		public static void print(Object[] arr) {
+			each(arr, printer);
+		}
+		
+		public static interface GroupIterator {
+			public void onNewGroup(Object k);
+			public void onNewEntry(Object v);
+		}
+		
+		public static GroupIterator groupPrinter = new GroupIterator() {
+			
+			@Override
+			public void onNewGroup(Object k) {
+				System.out.println(k);
+			}
+			
+			@Override
+			public void onNewEntry(Object v) {
+				System.out.println("\t" + v);
+			}
+		}; 
+		
+		public static void iterateOverGroup(HashMap<Object, Object> group, final GroupIterator i) {
+			each(group, new HashRunner() {
+				public void run(Object k, Object v) {
+					i.onNewGroup(k);
+					@SuppressWarnings("unchecked")
+					List<Object> l = (List<Object>) v;
+					each(l, new Runner() {
+						public void run(Object o) {
+							i.onNewEntry(o);
+						}						
+					});
+				}
+			});
+		}
+	}
+}
